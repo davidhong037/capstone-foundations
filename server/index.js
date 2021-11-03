1d94a1cd22155f43383a9522748642be5ead530c
David Hong <phazex1@gmail.com>
2021-11-03 00:18:57 -0400

    updating server/index.js

--- server/index.js
+++ server/index.js
@@ -19,8 +19,12 @@ app.get('/journey', function(req, res){
     res.sendFile(path.join(__dirname, "../client/journey.html"))
 })
 
-app.get('/journey', function(req, res){
-    res.sendFile(path.join(__dirname, "../client/terrorblade.html"))
+app.get('/boss-fight', function(req, res){
+    res.sendFile(path.join(__dirname, "../client/boss-fight.html"))
+})
+
+app.get('/feedback', function(req, res){
+    res.sendFile(path.join(__dirname, "../client/feedback.html"))
 })
 
 app.get('/css', function(req, res){
@@ -28,7 +32,15 @@ app.get('/css', function(req, res){
 })
 
 app.get('/js', function(req, res){
-    read.sendFile(path.join(__dirname, "../client/main.js"))
+    read.sendFile(path.join(__dirname, "../client/weapons.js"))
+})
+
+app.get('/js', function(req, res){
+    read.sendFile(path.join(__dirname, "../client/health-bar.js"))
+})
+
+app.get('/js', function(req, res){
+    read.sendFile(path.join(__dirname, "../client/feedback.js"))
 })
 
 const port = process.env.PORT || 4567
